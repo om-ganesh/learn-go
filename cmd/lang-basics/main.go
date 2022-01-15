92f86419fd656c4aec8dc9810642b32fef8a0dd9
Om Ganesh <dreamcodelife@gmail.com>
2022-01-15 05:32:43 -0500

    Import and Use the om-ganesh/goutils module

--- cmd/lang-basics/main.go
+++ cmd/lang-basics/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/om-ganesh/goutils"
+)
 
 func main() {
-    fmt.Println("This is Language Basics")
+	fmt.Println("Learn importing modules")
+	fmt.Println(goutils.Hello())
+	str := "Hello Kul Umesh !!!"
+	fmt.Println("AlphaNumeric ", str, "? ", goutils.IsAlphanumeric(str))
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/om-ganesh/learn-go
+
+go 1.17
+
+require github.com/om-ganesh/goutils v0.1.0
